e36862ec96c60b9378fa87b279220c9986a5c2f1
krtsk <krtsk.xo@gmail.com>
2023-02-20 20:36:36 +0300

    ln 25 strada

---
+++ ln25.js
@@ -0,0 +1,13 @@
+// forEach метод массивов, который проходит по всем его элементам и выполняет для каждого 
+// из них функцию, переданную в качестве аргумента
+const number = [1, 2, 3, 4, 5];
+number.forEach (console.log);
+number.forEach (alert);
+
+//const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+// const firstWord = animals.find (str.substr(start[7, length]));
+// console.log (animals);
+
+const numbers = [-1, -11, -2, -3, -10, -4];
+
+numbers.forEach (alert);
